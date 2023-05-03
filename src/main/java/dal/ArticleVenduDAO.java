71576f303bc9786c4817a150ded702819166fa1b
jollivier <jollivier@PO-OLLIVIER.interne.eseo.fr>
2023-05-03 14:06:00 +0200

    .dal - ArticleVenuDAO.java

--- src/main/java/dal/ArticleVenduDAO.java
+++ src/main/java/dal/ArticleVenduDAO.java
@@ -1,5 +1,19 @@
 package dal;
 
+import java.util.List;
+
+import bo.ArticleVendu;
+
 public class ArticleVenduDAO {
+	
+	public void insert(ArticleVendu articleVendu) throws DALException;
+	
+	public void update(ArticleVendu articleVendu) throws DALException;
+	
+	public void delete(int noArticle) throws DALException;
+	
+	public ArticleVendu selectById(Integer noArticle) throws DALException;
+	
+	public List<ArticleVendu> selectAll() throws DALException;
 	
 }
