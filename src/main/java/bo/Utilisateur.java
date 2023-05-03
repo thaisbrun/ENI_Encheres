f74afc64c91613e7ccefa164107be37ef16a18ee
jollivier <jollivier@PO-OLLIVIER.interne.eseo.fr>
2023-05-03 10:04:56 +0200

    Création classe Utilisateur.java

---
+++ src/main/java/bo/Utilisateur.java
@@ -0,0 +1,133 @@
+package bo;
+
+public class Utilisateur {
+	
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private int telephone;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private boolean administrateur;
+	
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, int telephone,
+			String rue, int codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+	
+	public Utilisateur() {}
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+	
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getTelephone() {
+		return telephone;
+	}
+	
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+	public String getRue() {
+		return rue;
+	}
+	
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	
+	public int getCodePostal() {
+		return codePostal;
+	}
+	
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
+	public int getCredit() {
+		return credit;
+	}
+	
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+	
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+	
+	
+}
